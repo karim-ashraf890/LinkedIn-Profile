c76409aeb26b7c166e2105651249048c706f39dc
karim <karimashraf890@outlook.com>
2025-08-20 19:08:18 +0300

    dropdown

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const data = [
     { id: 2, text: "Ashraf" },
     { id: 3, text: "Ali" }
 ];
-
 $(document).ready(function () {
     $(".js-example-placeholder-multiple").select2({
         data: data,
@@ -24,3 +23,44 @@ document.querySelector('.search-icon').addEventListener('click', function () {
         $('.js-example-placeholder-multiple').select2('open');
     }, 100); // استنى شوية بعد الظهور
 });
+var dropdownContainer = $('.dropdown');
+var dropdownMenu = $('.dropdown-menu');
+dropdownContainer.on('click', toggleDropdown);
+function toggleDropdown() {
+    if (dropdownMenu.css('display') === 'block') {
+        dropdownMenu.css('display', 'none');
+    } else {
+        dropdownMenu.css('display', 'block');
+    }
+}
+$(window).on('click', function (e) {
+    if (!e.target.closest('.dropdown')) {
+        dropdownMenu.css('display', 'none');
+    }
+});
+var dropdownContainer1 = $('.dropdown-1');
+var dropdownMenu1 = $('.dropdown-menu-1');
+dropdownContainer1.on('click', toggleDropdown1);
+function toggleDropdown1() {
+    if (dropdownMenu1.css('display') === 'block') {
+        dropdownMenu1.css('display', 'none');
+    } else {
+        dropdownMenu1.css('display', 'block');
+    }
+};
+$(window).on('click', function (e) {
+    if (!e.target.closest('.dropdown-1')) {
+        dropdownMenu1.css('display', 'none');
+    }
+});
+
+
+const forBusinessToggle = document.querySelector('.for-business-toggle');
+const dropdownForBusiness = document.querySelector('.dropdownForBusiness');
+
+forBusinessToggle.addEventListener('click', () => {
+    dropdownForBusiness.classList.toggle('active');
+});
+
+
+
